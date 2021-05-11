17d5b17ad8bc465e0983fb8a757de11e7a7e372d
Jakub Modzelik <jakub.modzelik@gmail.com>
2021-05-11 12:56:55 +0200

    first cmmit

---
+++ windows/react_project/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::react_project::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::react_project::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
